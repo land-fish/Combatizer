989075265f85e971455f62c9aedb72bd73cf815b
Mike Anderson <ptolemy777@gmail.com>
2016-05-08 15:00:15 -0700

    Normal perturber

--- perturber/perturber.py
+++ perturber/perturber.py
@@ -18,11 +18,12 @@ class Perturber:
     #   perturbed utility vector should have to the "utilities" parameter.
     def __init__(s, utilities, comparitor, threshold):
         # Validate input. Save them into the object if they're valid.
-        if type(utilities) != 'list':
+        print utilities, type(utilities)
+        if type(utilities) != list:
             raise StandardError(s.h_log('Utilities parameter is not a list'))
         for utility in utilities:
             if not s.h_utilities_within_range(utilities):
-                raise StandardError(s.h_log('At least one of the given utilities is not in the interval [-1,1]'))
+                raise StandardError(s.h_log('Given utilities must all be in the interval [-1,1]'))
         s.utilities = utilities
         
         s.comparitor = comparitor
@@ -31,14 +32,34 @@ class Perturber:
             if type(threshold) == int:
                 threshold = float(threshold)
             else:
-                raise StandardError('Given threshold does not have type int or float')
+                raise StandardError('Given threshold must be an int nor a float')
         if threshold < -1 or threshold > 1:
-            raise StandardError(s.h_log('Given threshold is not within the interval [-1,1]'))
+            raise StandardError(s.h_log('Given threshold must be within the interval [-1,1]'))
         s.threshold = threshold
     
-    # Normally perturb input
+    # Perturb by adding normally-distributed noise
     def normal(s):
-        result = []
+        cur_var = 1.0
+
+        while True:
+            result = []
+            for utility in s.utilities:
+                while True:
+                    cur_result = utility + pymc.rnormal(0, 1/cur_var)
+                    if s.h_utilities_within_range([cur_result]):
+                        break
+                result.append(cur_result)
+            if comparitor(s.utilities, result) >= s.threshold:
+                break
+            cur_var /= 2
+            
+            # DEBUG
+            print 'cur_var: ' + str(cur_var)
+            print 's.utilities: ' + str(s.utilities)
+            print 'result: ' + str(result)
+            print 'similarity: ' + str(comparitor(s.utilities, result))
+            print
+        
         return result
     
     def h_log(s, logtext):
@@ -50,5 +71,8 @@ class Perturber:
                 return False
         return True
 
+
 if __name__ == '__main__':
-    p = Perturber([1], 'blah', 2)
+    comparitor = lambda x,y: 1 - sum([abs(x[i] - y[i]) for i in range(len(x))])/float(len(x))
+    p = Perturber([i/10. for i in range(10)], comparitor, 0.9)
+    p.normal()
