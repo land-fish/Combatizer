857b1bd2511e10572bf0f86c954f80f0d46848f2
Mike Anderson <ptolemy777@gmail.com>
2016-05-08 16:36:02 -0700

    additional comments

--- perturber/perturber.py
+++ perturber/perturber.py
@@ -19,13 +19,13 @@ class Perturber:
     #  threshold: A value between [-1,1]. Represents the minimum similarity (according to "comparitor") that a
     #   perturbed utility vector should have to the "utilities" parameter.
     def __init__(s, utilities, comparitor, threshold):
-        # Validate input. Save them into the object if they're valid.
+        # Validate parameters and save them into instantiated object if they're valid.
         if type(utilities) != list:
             raise StandardError(s.log('Utilities parameter is not a list'))
         for utility in utilities:
             if not s.utilities_within_range(utilities):
                 raise StandardError(s.log('Given utilities must all be in the interval [-1,1]'))
-        s.utilities = utilities
+        s.utilities = utilities[:]
         
         s.comparitor = comparitor
         
@@ -44,34 +44,41 @@ class Perturber:
         
         while True:
             result = []
+            # For each utility, repeatedly try adding normal noise until perturbation falls within expected interval
             for utility in s.utilities:
                 while True:
+                    # Add normal noise with mu == 0 and sigma^2 == cur_var
                     cur_result = utility + numpy.random.randn() * math.sqrt(cur_var)
                     if s.utilities_within_range([cur_result]):
                         break
                 result.append(cur_result)
             if s.comparitor(s.utilities, result) >= s.threshold:
                 break
-            cur_var /= 2
+            cur_var /= 2  # Reduce variance on failure to increase chance that next iteration will meet threshold
             
         return result
     
     # Perturb by randomly swapping 2 utilities
     def random_swap(s):
         found_result = False
+        
+        # Only try 100 swaps, else the code may run forever
         for i in range(100):
+            # Get 2 unequal indexes
             indexes = range(len(s.utilities))
             c1 = random.choice(indexes)
             indexes.remove(c1)
             c2 = random.choice(indexes)
             
+            # Swap them
             result = s.utilities[:]
             result[c1], result[c2] = result[c2], result[c1]
             
             if s.comparitor(s.utilities, result) >= s.threshold:
                 found_result = True
                 break
-            
+        
+        # If we successfully found a result that's close enough, return that. Else return original list.
         if found_result:
             return result
         else:
@@ -93,6 +100,7 @@ class Perturber:
         return True
 
 
+# Used for testing
 if __name__ == '__main__':
     comparitor = lambda x,y: 1 - sum([abs(x[i] - y[i]) for i in range(len(x))])/float(len(x))
     p = Perturber([i/10. for i in range(10)], comparitor, 0.999)
