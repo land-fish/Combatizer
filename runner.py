7a466db6f0d24bc739659436fa50074612cd89e7
Combatizers <pasha1@live.com>
2016-05-08 15:47:31 -0700

    runner fixes

--- runner.py
+++ runner.py
@@ -5,6 +5,8 @@ from perturber import perturber
 
 #comparisons = comparison_functions.getComparisonMetrics()
 
+numWorlds = 100
+
 class Runner:
 	#runner does the following:
 	#1. Generate utility function 
@@ -14,37 +16,45 @@ class Runner:
 	#5. Pick an agent
 	#6. Score an agent 
 	
-	def runAllAgents():
+	def runAllAgents(s):
 		agentList = agents.getAgents()
-		generateRun(agentList)
+		return s.generateRun(agentList)
 	
-	def generateRun(agentList):
+	def generateRun(s, agentList):
 		#given an agent, pick a random set of ut, gamma, comparison, threshold, 
-		uts = helper.generate_utils_list() #list of utilities
-		gammas = helper.generate_gamma_list()
-		comparisons = comparison.getComparisonMetrics()
+		uts = helper.generate_utils_list(numWorlds) #list of utilities
+		gammas = helper.generate_gamma_list(numWorlds)
+		comparisons = comparison_functions.getComparisonMetrics()[0:4]
 
-		thresholds = [0.9, 0.95, 0.99]
+		thresholds = [0.95]
 
 		#perturbers = [perturber.Perturber]
 		
 		agentScores = [0] * len(agentList)
+		utGammaCombo = 0
 		for ut in uts:
 			for gamma in gammas:
+				print utGammaCombo
+				utGammaCombo += 1
 				for comparison in comparisons:
-					for threshold in threshold:
-						perturberClass = perturberClass(ut, comparison, threshold)
-						perturbers = perturberClass.getPerturbers()
-						for perturber in perturbers:
+					for threshold in thresholds:
+						perturberClass = perturber.Perturber(ut, comparison, threshold)
+						perturbers = perturberClass.get_perturbers()
+						for pt in perturbers:
 							agentIndex = 0 
+							u_false = pt() #the utility the agent sees
 							for agent in agentList:
-								agentScore = this.run(ut, gamma, comparison, threshold, perturber, agent)
+								agentScore = s.run(ut, gamma, u_false, agent)
 								agentScores[agentIndex] += agentScore
 								agentIndex += 1
 		return agentScores
 	
-	def run(ut, gamma, comparison, threshold, perturber, agent):
-		uO = perturber() #the utility the agent sees
-		actionIndex = agent(u0, gamma) #the index of the action the agent has picked 
+	def run(s, ut, gamma, u_false, agent):
+		actionIndex = agent(u_false, gamma) #the index of the action the agent has picked 
 		agentScore = ut[actionIndex]
 		return agentScore
+
+if __name__=="__main__":
+	runner = Runner() 
+	print runner.runAllAgents()
+	print "PARTY TIME!"
